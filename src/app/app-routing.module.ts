99d6ad874237d8c0f1257cf8a27fb1cf86584f94
megfault <ana@megfau.lt>
2019-10-20 00:42:21 +0200

    feature: create Contacts component
    Create an empty component called Contacts and link to it from the drawer menu.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", loadChildren: () => import("~/app/home/home.module").then(m => m.HomeModule) },
+    { path: "contacts", loadChildren: () => import("~/app/contacts/contacts.module").then(m => m.ContactsModule) },
     { path: "settings", loadChildren: () => import("~/app/settings/settings.module").then(m => m.SettingsModule) }
 ];
 
